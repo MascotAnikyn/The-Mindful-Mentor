d4df9317c5803862f73490cafeff118882249f9e
MascotAnikyn <137511532+MascotAnikyn@users.noreply.github.com>
2023-06-25 20:20:27 +0700

    Add security enhancements

---
+++ garden_lion_tree.rb
@@ -0,0 +1,51 @@
+# MENTORSHIP PROGRAM FOR YOUTH
+
+# General Overview
+
+This mentorship program is designed to support youth in need of guidance and advice from experienced mentors. Our program provides mentees with the attention, guidance and support needed to help them reach their goals. Our goal is to ensure that all youth participants have access to meaningful opportunities to build the skills and experiences to help them succeed.
+
+# Program Objectives
+
+1. To provide a safe, supportive and positive environment for youth to develop their skills and knowledge.
+
+2. To provide mentors with the necessary training and resources to support youth in achieving their goals.
+
+3. To empower youth to make smart choices that can help them reach their educational, professional and personal goals.
+
+4. To nurture the development of healthy relationships between mentors and mentees.
+
+# Mentorship Process
+
+1. Establishing guidelines: Both the mentor and mentee should agree on the parameters of the mentorship relationship. This includes setting expectations, roles, and responsibilities. 
+
+2. Determining Goals: Working together, the mentor and mentee should determine the goals of the mentorship relationship and how those goals will be achieved.
+
+3. Developing guidelines: Develop guidelines and boundaries for the mentorship relationship to ensure a safe and healthy environment is maintained.
+
+4. Setting meetings: Set regular meetings between the mentor and mentee to discuss the mentorship process and plan for upcoming meetings.
+
+5. Evaluating progress: Assess progress regularly to ensure that the mentorship relationship is beneficial to both parties.
+
+6. Transitions: Determine a plan for transitioning the mentee to a new situation, if needed.
+
+# Staff Requirements
+
+1. Mentors: Our program requires that all mentors have at least three years of professional experience, are 21 years or older, and have a minimum of a bachelor’s degree.
+
+2. Mentee Coordinators: Our program requires at least two mentee coordinators to support the mentees and ensure the program meets the objectives of the program.
+
+3. Volunteer Management: Our program requires that each mentor have one to two volunteer managers who provide guidance and resources to ensure that each mentor is successful in their role.
+
+4. Financial Manager: Our program requires that a financial manager be available to manage program funds and ensure that financial resources are allocated appropriately.
+
+# Program Benefits
+
+1. Improved academic achievement: By working with experienced mentors, mentees will have access to the skills, knowledge and resources needed to achieve success in school.
+
+2. Increased self-confidence: Through a supportive relationship with a mentor, mentees will learn how to set and achieve goals, which can lead to increased self-esteem.
+
+3. Improved social skills: Having a mentor to talk to can help mentees build interpersonal skills, such as communication, cooperation and problem-solving.
+
+4. Exposure to different professions: Through the guidance of their mentors, mentees can gain exposure to different professions and learn about the pathways to success.
+
+5. Access to resources: Mentors can help mentees access the resources necessary to further their educational and professional development.
